5f56ae7aa0d3794aff199ef980ab43d69e3bba7c
biQvOid <70909971+biQvOid@users.noreply.github.com>
2021-09-20 01:25:48 +0300

    Update octagon.cpp

--- lab1/octagon.cpp
+++ lab1/octagon.cpp
@@ -1,17 +1,17 @@
 #include "octagon.h"
 
-octagon::octagon(std::istream& is)
+Octagon::Octagon(std::istream& is)
 {
     std::cin >> a_ >> b_ >> c_ >> d_;
     std::cin >> e_ >> f_ >> g_ >> h_;
 }
 
-size_t octagon::VertexesNumber()
+size_t Octagon::VertexesNumber()
 {
     return (size_t)8;
 }
 
-double octagon::Area()
+double Octagon::Area()
 {
     return 0.5 * abs((a_.getX() * b_.getY() + b_.getX() * c_.getY() + c_.getX() * d_.getY() + d_.getX() * e_.getY() + e_.getX() * f_.getY() +
     f_.getX() * g_.getY() + g_.getX() * h_.getY() + h_.getX() * a_.getY() - (b_.getX() * a_.getY() + c_.getX() * b_.getY() + 
@@ -19,7 +19,7 @@ double octagon::Area()
     a_.getX() * h_.getY())));
 }
 
-void octagon::Print(std::ostream& os)
+void Octagon::Print(std::ostream& os)
 {
     std::cout << "Octagon: " << a_ << " " << b_ << " ";
     std::cout << c_ << " " << d_ << " " << e_ << " ";
