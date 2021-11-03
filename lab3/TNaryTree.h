f484438287f88ab3c240a2d2970c449c0370b8fa
biQvOid <70909971+biQvOid@users.noreply.github.com>
2021-11-04 01:24:34 +0300

    Update TNaryTree.h

--- lab3/TNaryTree.h
+++ lab3/TNaryTree.h
@@ -1,6 +1,3 @@
-#ifndef TNARY_TREE
-#define TNARY_TREE
-
 #include "octagon.h"
 #include "TNaryTree_item.h"
 #include <memory>
@@ -12,17 +9,17 @@ public:
     TNaryTree(const TNaryTree& other);
     TNaryTree();
 
-    void Update(const octagon &&polygon, const std::string &&tree_path)
+    void Update(const std::shared_ptr<octagon> &&polygon, const std::string &&tree_path)
     {
         Update(&root, polygon, tree_path);
     }
 
-    void Update(const octagon &polygon, const std::string &tree_path)
+    void Update(const std::shared_ptr<octagon> &polygon, const std::string &tree_path)
     {
         Update(&root, polygon, tree_path);
     }
 
-    const octagon& GetItem(const std::string& tree_path)
+    const std::shared_ptr<octagon>& GetItem(const std::string& tree_path)
     {
         return GetItem(&root, tree_path);
     }
@@ -38,8 +35,8 @@ public:
 private:
     int size;
     std::shared_ptr<TreeItem> root;
-    void Update(std::shared_ptr<TreeItem>* root, octagon polygon, std::string tree_path);
-    const octagon& GetItem(std::shared_ptr<TreeItem>* root, const std::string tree_path);
+    void Update(std::shared_ptr<TreeItem>* root, std::shared_ptr<octagon> polygon, std::string tree_path);
+    const std::shared_ptr<octagon>& GetItem(std::shared_ptr<TreeItem>* root, const std::string tree_path);
 };
 
 #endif
